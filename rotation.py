27a4456c9bd44fcb1dd412af7230e5529cb2aff2
Gereon Feldmann <feldmann.gereon@gmail.com>
2022-02-01 16:07:02 +0100

    first try print coord

--- rotation.py
+++ rotation.py
@@ -1,1 +1,18 @@
-print("Hello World?")
+import pandas as pd
+import numpy as np
+import csv
+
+
+
+
+df = pd.DataFrame(['N',0.000000,0.00000,1.100000],
+                  ['N',0.000000,0.00000,0.00000]).T
+                  
+with open('out.xyz', 'w') as f:
+    # create the csv writer
+    writer = csv.writer(f)
+    print(len(df))
+    # write a row to the csv file
+    writer.writerow(f'{len(df)}')
+
+df.to_csv('coord.xyz',mode = 'a',index = False)
