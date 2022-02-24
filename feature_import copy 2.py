efc81814b7f85063be8d92effa230b8c640f01e0
Gereon Feldmann <feldmann.gereon@gmail.com>
2022-02-24 13:48:32 +0100

    bug fix

--- feature_import copy 2.py
+++ feature_import copy 2.py
@@ -1,6 +1,8 @@
 from calendar import c
 from msilib.schema import Feature
 from turtle import shape
+
+from sklearn import preprocessing
 from functions import *
 from operator import matmul
 from xml.etree import ElementInclude
@@ -17,7 +19,8 @@ import matplotlib.pyplot as plt
 
 
 from sklearn.ensemble import RandomForestRegressor
-
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import  Matern ,RBF
 from sklearn.model_selection import train_test_split
 
 def box_plot(X,y,thresh,file):
@@ -48,7 +51,7 @@ def box_plot(X,y,thresh,file):
 
 #############################
 #Init File Import for Information
-file_path = glob.glob('h2/H2_*/')
+file_path = glob.glob('tests/h2_2/H2_*/')
 init_path_coord = f'{file_path[0]}'+'coord.xyz'
 coord,head = import_coord(init_path_coord)
 Nat = len(coord.iloc[:,0])
@@ -122,19 +125,23 @@ for file in range(len(file_path)):
 ###############################
 #Generating the specific feature for diagonal matrix blocks
 X = np.zeros([len(file_path)*Nat*(3)**2,43])
-y = np.zeros(len(file_path*Nat*(3)**2))
+y = np.zeros(len(file_path)*Nat*(3)**2)
 k = 0
 for file in range(len(file_path)):
-    for i in range(3):
-        for j in range(3):
-            for A in range(Nat):
+    for A in range(Nat):
+        for i in range(3):
+            for j in range(3):
                 xyz = np.array([0,0,0])
                 xyz[i] += 1
                 xyz[j] += 1
                 X[k,0:3] = xyz
                 X[k,3:]  = Feature_Coll[file,A]
-                y[k] = Hessian_Coll[file,i,j]
+                y[k] = Hessian_Coll[file,i+3*A,j+3*A]
                 k+=1
+
+
+
+#X = preprocessing.normalize(X,norm ='l2')
 
 
 
@@ -149,11 +156,13 @@ y_test =  y[36:54]
 #X_test,y_train,y_test  #  = train_test_split(X, y, test_size=0.25 ,random_state=42)
 
 regr_diag = RandomForestRegressor(n_estimators = 100,random_state=42)
+#regr_diag = GaussianProcessRegressor(kernel = Matern() ,random_state=42)
+
 
 regr_diag.fit(X_train, y_train)
 
-print(regr_diag.score(X_test,y_test))
-
+print('Score of diag model:',regr_diag.score(X_test,y_test))
+"""
 importances = regr_diag.feature_importances_
 
 ticks = np.array(range(len(X[0])))
@@ -176,68 +185,80 @@ ax.set_xticklabels(['x','y','z']+col_name,rotation=90)
 
 plt.gcf().subplots_adjust(bottom=0.45)
 fig.set_figwidth(15)
-
+"""
 
 X_non_diag = np.zeros([len(file_path)*Nat**2*(3)**2,203])
 y_non_diag = np.zeros(len(file_path*Nat**2*(3)**2))
 k = 0
+
 for file in range(len(file_path)):
-    for i in range(3):
-        for j in range(3):
-            for A in range(Nat):
-                for B in range(Nat):
-                    #if A < B:
-                    xyz = np.array([0,0,0])
-                    xyz[i] += 1
-                    xyz[j] += 1
-                    X_non_diag[k,0:3] = xyz
-                    X_non_diag[k,3:43] = Feature_Coll[file,A]
-                    X_non_diag[k,43:83]= Feature_Coll[file,B]
-                    X_non_diag[k,83:123]= (Feature_Coll[file,A] +Feature_Coll[file,B])/2
-                    X_non_diag[k,123:163]= (Feature_Coll[file,B]*Feature_Coll[file,A])
-                    X_non_diag[k,163:203]= np.abs(Feature_Coll[file,B] -Feature_Coll[file,A])
+    for A in range(Nat):
+        for B in range(Nat):
+            for i in range(3):
+                for j in range(3):
+                        #if A < B:
+                        #print('i,j=',i,j)
+                        #print('A,B=',A,B)
+                        xyz = np.array([0,0,0])
+                        xyz[i] += 1
+                        xyz[j] += 1
+
+                        X_non_diag[k,0:3] = xyz
+                        X_non_diag[k,3:43] = Feature_Coll[file,A]
+                        X_non_diag[k,43:83]= Feature_Coll[file,B]
+                        X_non_diag[k,83:123]= (Feature_Coll[file,A] +Feature_Coll[file,B])/2
+                        X_non_diag[k,123:163]= (Feature_Coll[file,B]*Feature_Coll[file,A])
+                        X_non_diag[k,163:203]= np.abs(Feature_Coll[file,B] -Feature_Coll[file,A])
 
 
-                    y_non_diag[k] = Hessian_Coll[file,i,j]
-                    k+=1
-
-
+                        y_non_diag[k] = Hessian_Coll[file,i+3*A,j+3*B]
+                        k+=1
+'''
+for k in range(3*Nat):
+    for l in range(3*Nat):
+            k = i%Nat
+            l = j%Nat
+            print('k,l=',k,l)
+            C = k%3
+            D = l%3
+            print('C,D=',C,D)
+'''
 #X_non_diag_train, X_non_diag_test, y_non_diag_train, y_non_diag_test = train_test_split(X_non_diag, y_non_diag, test_size=0.25 ,random_state=42)
+#X_non_diag = preprocessing.normalize(X_non_diag,norm ='l2')
 
 X_non_diag_test = X_non_diag[36:72]
 X_non_diag_train = np.append(X_non_diag[:36],X_non_diag[72:],axis = 0)
 
 
 y_non_diag_test = y_non_diag[36:72]
-
 y_non_diag_train = np.append(y_non_diag[:36],y_non_diag[72:],axis = 0)
 
-regr_non_diag = RandomForestRegressor(n_estimators = 100,random_state=42)
+regr_non_diag = RandomForestRegressor(n_estimators = 100,bootstrap=False,random_state=42)
+#regr_non_diag = GaussianProcessRegressor(kernel = Matern(),random_state=42)
 
 regr_non_diag.fit(X_non_diag_train, y_non_diag_train)
 
-hess_non_diag_pred = regr_non_diag.predict(X_non_diag_test)
 
+
+
+hess_non_diag_pred = regr_non_diag.predict(X_non_diag_test)
 hess_diag_pred = regr_diag.predict(X_test)
+
+print('Score of non diag model:',regr_non_diag.score(X_non_diag_test,y_non_diag_test))
 
 hess_diag = np.zeros([3,3])
 k = 0
 for i in range(3):
     for j in range(3):
         hess_diag[i,j] = hess_diag_pred[k]
-        k += 2
-
-print(hess_diag)
-
+        k += 1
 
 hess_non_diag = np.zeros([3,3])
 k = 0
 for i in range(3):
     for j in range(3):
         hess_non_diag[i,j] = hess_non_diag_pred[k]
-        k += 4
-
-print(hess_non_diag)
+        k +=1
 
 hess = np.zeros([6,6])
 
@@ -246,13 +267,10 @@ hess[3:6,3:6] = hess_diag
 hess[0:3,3:6] = hess_non_diag
 hess[3:6,0:3] = hess_non_diag
 
-
-
 hessian_mass = mass_weighted_hessian(hess,coord['atoms'])
 lamb, Q = linalg.eigh(hessian_mass)
 freq1 = (np.sqrt(abs(lamb))/(atomic_time_unit*2*np.pi*speed_of_light))
 print(freq1)
-
 
 hessian_mass = mass_weighted_hessian(Hessian_Coll[1],coord['atoms'])
 lamb, Q = linalg.eigh(hessian_mass)
