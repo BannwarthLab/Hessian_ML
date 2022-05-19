6af12c596f9b80afc143af1835beb74ff70d5ab5
Gereon Feldmann <feldmann.gereon@gmail.com>
2022-05-19 16:37:32 +0200

    report update

---
+++ version_chk.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import sklearn as sk
+import matplotlib as plt
+
+
+print('pd:',pd.__version__)
+print('np:',np.__version__)
+print('sk:',sk.__version__)
+print('plt:',plt.__version__)
