fcae728a7fc0ba2d3e0261e5dd4322067aafd4f1
Gereon Feldmann <feldmann.gereon@gmail.com>
2023-04-06 15:17:43 +0200

    restructured folder part4

--- tests/temp/H3/do_init_hess.sh
+++
@@ -1,9 +0,0 @@
-for file in H3_*/
-do 
-	cd $file
-	cd init_coord
-		xtb coord.xyz --chrg 0 --etemp 300 --hess --gfn2
-                ~/git/xtb/xtb_ml/build/xtb coord.xyz --chrg 0 --etemp 300 --ml_feature
-	cd ..
-	cd ..
-done
