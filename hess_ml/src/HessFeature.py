bug fix regarding rotation for A,B change

hess_ml/src/HessFeature.py
@@ -29,19 +29,49 @@ class HessFeature(Rotation_Functions):
 
                 self.check_list.append([A,B])
 
-                if linalg.norm(self.dipm_atom[A]) < linalg.norm(self.dipm_atom[B]):
+
+
+
+                #Performs a rotation around the X axis by 180 ° if nuclear charge of A is smaller than B to achieve a consistent alignment
+                # If A == B rotation depends on dipole moment 
+                if self.nuc_charge[A] < self.nuc_charge[B]:
+
                     B,A = A,B
+
                     self.transpose_list.append([B,A])
+
                     rot_Mat = self.rot_X(np.pi)
-    
+
                     R_MI_APF = matmul(rot_Mat,R_MI_APF)
+
+                    rot_Mat = self.rot_Z(np.pi)
+
+                    R_MI_APF = matmul(rot_Mat,R_MI_APF)
+
+                elif self.nuc_charge[A] == self.nuc_charge[B]:
+
+                    if linalg.norm(self.dipm_atom[A]) < linalg.norm(self.dipm_atom[B]):
+
+                        B,A = A,B
+
+                        self.transpose_list.append([B,A])
+
+                        rot_Mat = self.rot_X(np.pi)
+
+                        R_MI_APF = matmul(rot_Mat,R_MI_APF)
+
+                        rot_Mat = self.rot_Z(np.pi)
+                        
+                        R_MI_APF = matmul(rot_Mat,R_MI_APF)
+
+
 
                 R_AB = linalg.norm(self.xyz.iloc[A,1:] - self.xyz.iloc[B,1:])
 
                 Quantity_AB = [[],[]]
 
-                j = 0
 
+                j = 0
                 for atom in [A,B]:
                     #____Rotation from initial coordinate system to atom pair focused system____
 

hess_ml/src/Rotation_func.py
@@ -72,8 +72,9 @@ class Rotation_Functions:
                
           axis = np.identity(3)
 
-
+          
           T = 1/2 * coord_end.iloc[i,1:] + 1/2 * coord_end.iloc[j,1:]
+          
           vec_dipm = dipm.iloc[i,1:] + dipm.iloc[j,1:]#np.sum(dipm.iloc[:,1:])/len(dipm.iloc[:,1:])#
 
           coord_end = self.vec_trans(coord_end,T)