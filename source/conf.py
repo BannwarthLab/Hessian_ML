a1824a32df9ca6843990f77ea02e96e049093174
Gereon Feldmann <feldmann.gereon@gmail.com>
2026-02-02 20:57:36 +0100

    Hotfix docs

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,7 @@
+import os 
+import sys 
+
+sys.path.insert(0, os.path.abspath('../src/mlhess'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -26,3 +30,13 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',    # Google / NumPy style docstrings
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+]
+
+autosummary_generate = True
